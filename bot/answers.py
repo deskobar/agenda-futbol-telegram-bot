ec0e1c55ffeea821fb9ea3a6cc00a38bdaa17535
David Escobar <david.escobar@ug.uchile.cl>
2024-09-25 19:34:52 -0300

    Fix decorator

--- bot/answers.py
+++ bot/answers.py
@@ -53,7 +53,7 @@ ALL_WITH_NO_COINCIDENCES = """
 # /version
 
 VERSION = """
-    3.0.0
+    3.1.0
 """
 
 # /set_alias
--- bot/decorator.py
+++ bot/decorator.py
@@ -3,19 +3,18 @@ from telebot.types import Message
 from bot import bot
 from bot.answers import WAKING_UP
 
-# Async decorator
+
 def db_waking_up(func):
     @wraps(func)
-    async def wrapper(message: Message, *args, **kwargs):
+    async def wrapper(*args, **kwargs):
         try:
+            message: Message = args[0]
             chat_id = message.chat.id
             message = await bot.reply_to(message, WAKING_UP)
-            result = await func(message, *args, **kwargs)
-            bot.delete_message(chat_id, message.message_id)
+            result = await func(*args, **kwargs)
+            await bot.delete_message(chat_id, message.message_id)
             return result
         except Exception as e:
-            print(f"An error occurred: {e}")
-        finally:
-            print("After the function call")
+            print(f"An error occurred: {str(e)}")
 
     return wrapper
