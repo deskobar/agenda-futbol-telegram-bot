2e2c41990718458ce9144ef115171a181bf37694
David Escobar <david.escobar@ug.uchile.cl>
2024-09-26 00:54:53 -0300

    Use RAM efficiently

--- bot/common.py
+++ bot/common.py
@@ -5,23 +5,27 @@ from bot import bot
 
 import io
 from PIL import Image
+import uuid
 
 
 def df_to_image(df: pd.DataFrame, dpi: int = 300) -> io.BytesIO:
-    fig, ax = plt.subplots(figsize=(df.shape[1] * 2, df.shape[0] * 0.5))
+    plt.ioff()
+    fig = plt.figure(figsize=(df.shape[1] * 2, df.shape[0] * 0.5), num=uuid.uuid4().hex)
+    ax = fig.gca()
     ax.axis('off')
     table = ax.table(cellText=df.values, colLabels=df.columns, cellLoc='center', loc='center')
     table.scale(1, 1.5)
 
-    with io.BytesIO() as image_stream:
-        plt.savefig(image_stream, format='png', bbox_inches='tight', dpi=dpi, transparent=True)
-        image_stream.seek(0)
-        with Image.open(image_stream) as image:
-            optimized_stream = io.BytesIO()
-            image.save(optimized_stream, format='PNG', optimize=True)
-        optimized_stream.seek(0)
-    plt.close()
-    return optimized_stream
+    image_stream = io.BytesIO()
+    fig.savefig(image_stream, format='png', bbox_inches='tight', dpi=dpi, transparent=True)
+    image_stream.seek(0)
+
+    with Image.open(image_stream) as image:
+        image.save(image_stream, format='PNG', optimize=True)
+
+    image_stream.seek(0)
+    plt.close(fig)
+    return image_stream
 
 
 async def send_img_or_msg_if_no_content(
